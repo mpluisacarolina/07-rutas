f87e891303f26fe05aff1010b20c2b6e83d9b09a
mpluisacarolina <mpluisacarolina@gmail.com>
2024-01-16 15:38:34 -0400

    Inicio del Menú de Navegación

--- src/routers/RouterPrincipal.jsx
+++ src/routers/RouterPrincipal.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import { Routes, Route, Link, BrowserRoute, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, NavLink } from 'react-router-dom';
 import { Index } from '../components/Index';
 import { Contacto } from '../components/Contacto';
 import { Articulos } from '../components/Articulos';
 import { Error } from '../components/Error'
- 
+
 export const RouterPrincipal = () => {
   return (
     <BrowserRouter>
+      <h1>Cabecera</h1>
+      <hr />
+
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/Index" >Index</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Articulos" >Artículos</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contacto" >Contacto</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="contenido-principal">
         <Routes>
-            <Route path="/" element={<Index/>} />
-            <Route path="index" element={<Index/>} />
-            <Route path="contacto" element={<Contacto/>} />
-            <Route path="articulos" element={<Articulos/>} />
-            <Route path="*" element={<Error/>} />
+          <Route path="/" element={<Index />} />
+          <Route path="index" element={<Index />} />
+          <Route path="contacto" element={<Contacto />} />
+          <Route path="articulos" element={<Articulos />} />
+          <Route path="*" element={<Error />} />
         </Routes>
+      </section>
+
+
+      <hr />
+      <footer>
+        Este es el pie de Página
+      </footer>
 
     </BrowserRouter>
   )
